35a7cd6db02dba87acef08ab3ef3a6e37a3ec99b
RUNAP <93928467+RUNAP@users.noreply.github.com>
2024-04-21 04:07:14 +0900

    Add files via upload

---
+++ 1_hoisting.js
@@ -0,0 +1,17 @@
+//변수 선언이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상.
+
+// var name2;
+
+// console.log(name2);
+
+// name2 = '코드2'
+
+// console.log(name2);
+
+// console.log(ljh);
+// let ljh = '정호';
+
+console.log(ljh2);
+const ljh2 = '정호2';
+
+//var = undefined let,const = Cannot acces =>var은 hoisiting를 막아주지못함.
